4c4c43194c25b93e832d5a6cf5eea679a03252e2
daichi <blue.blue.13.apple@gmail.com>
2025-08-26 12:19:54 +0900

    fix: Vercel adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/server';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: []
 });
